00ce57fc245bdea8681141dbeaa79cb2105e4cf1
Andrés Chacón <124219246+Achac04@users.noreply.github.com>
2024-04-21 22:36:26 -0600

    Firebase

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -8,12 +8,12 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore, collection} from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCoYS-pdTdwccsWGVIlS459NOSji0BYWLA',
-  authDomain: 'aceaccounting-7707c.firebaseapp.com',
-  projectId: 'aceaccounting-7707c',
-  storageBucket: 'aceaccounting-7707c.appspot.com',
-  messagingSenderId: '200319907616',
-  appId: '1:200319907616:web:6baf1fe636785693840945',
+  apiKey: "AIzaSyCoYS-pdTdwccsWGVIlS459NOSji0BYWLA",
+  authDomain: "aceaccounting-7707c.firebaseapp.com",
+  projectId: "aceaccounting-7707c",
+  storageBucket: "aceaccounting-7707c.appspot.com",
+  messagingSenderId: "200319907616",
+  appId: "1:200319907616:web:6baf1fe636785693840945"
 };
 
 // Initialize Firebase
